af45efd3fbd9e264cb06f5d119d8bf5daa652e33
Jonathan LAURENT <johjo@ytreza.org>
2024-11-26 14:26:59 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test.spec.ts

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -3,6 +3,17 @@ import {expect} from "vitest";
 describe('test', () => {
 
     it('XXX',()=>{
-        expect("a").toEqual("B")
+        expect("a").toEqual("a")
     })
 })
+
+
+// 1 - une cellule vivante ayant moins de deux cellules voisines vivantes va mourir par sous population
+// 2 - une cellule vivante ayant deux ou trois cellules voisines vivantes va survivre
+// 3 - une cellule vivante ayant plus de trois cellules voisines vivantes va mourir par surpopulation
+// 4 - une cellule morte ayant exactement trois cellules voisines vivantes va revenir à la vie, par reproduction
+
+// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+//   Any live cell with two or three live neighbours lives on to the next generation.
+//   Any live cell with more than three live neighbours dies, as if by overpopulation.
+//   Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
